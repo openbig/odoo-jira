8f038ae373342ecc095064720dfd9709e8fc8760
akulpa <akulpa@cirrus.pl>
2015-02-20 11:59:28 +0100

    sync of priorities, comments, attachments

--- project_issue_jira.py
+++ project_issue_jira.py
@@ -4,7 +4,9 @@ from openerp.tools.translate import _
 import logging
 import openerp.exceptions
 from jira_config import JIRA_SERVER, JIRA_PROTOCOL, JIRA_USERNAME, JIRA_PASSWORD, jira_configuration
-
+from openerp import SUPERUSER_ID
+import string
+import os
 
 class project_issue_jira(osv.osv):
 
@@ -16,7 +18,40 @@ class project_issue_jira(osv.osv):
     'jira_key': fields.char('JIRA key', size=256, readonly=True),
     'jira_status': fields.char('JIRA status', size=30, readonly=True),
     'jira_link': fields.char('JIRA Issue Link', size=256, readonly=True),
+    'jira_comments': fields.char('JIRA Comments', size=256, readonly=True),
+    'jira_attachments': fields.char('JIRA Attachments', size=256, readonly=True),
   }
+
+  def get_task_id(self, cr, uid, jira_status, context=None):
+    query = str(jira_status)+"%"
+    task_type = self.pool.get('project.task.type').search(cr, uid, [('name', 'like', query)])
+    if task_type:
+	task_type_id = self.pool.get('project.task.type').browse(cr, uid, task_type, context=context)
+
+    return task_type_id.id
+
+
+  def case_escalate(self, cr, uid, ids, context=None):        # FIXME rename this method to issue_escalate
+    for issue in self.browse(cr, uid, ids, context=context):
+        data = {}
+        esc_proj = issue.project_id.project_escalation_id
+        if not esc_proj:
+            raise osv.except_osv(_('Warning!'), _('You cannot escalate this issue.\nThe relevant Project has not configured the Escalation Project!'))
+
+        data['project_id'] = esc_proj.id
+        if esc_proj.user_id:
+            data['user_id'] = esc_proj.user_id.id
+        issue.write(data)
+
+        if issue.task_id:
+            issue.task_id.write({'project_id': esc_proj.id, 'user_id': False})
+
+	###After escalation the stage changes to Open
+	###task_type_id=pj.get_task_id(cr, uid, "Open", context=None)
+	task_type_id=self.get_task_id(cr, uid, "Open", context=None)
+	issue.write({'stage_id': task_type_id})
+
+    return True
 
 
 class project_jira(osv.osv):
@@ -95,6 +130,85 @@ class project_jira(osv.osv):
 	return 0
 
   def sync_project(self, cr, uid, project_issue_ids, jira_project_key, jira, context=None):
+	stagemap = {
+            "Open (JIRA)":"Open",
+            "In Progress (JIRA)":"In Progress",
+	    "Reopened (JIRA)":"Reopened",
+	    "Resolved (JIRA)":"Resolved",
+	    "Closed (JIRA)":"Done",
+	    "Cancelled":"Cancelled",
+	    "Done":"Done"
+        }
+
+	prioritymap = {
+	    "0":"Minor",
+	    "1":"Major",
+	    "2":"Critical"
+	}
+
+
+	def _get_attachments(issue_id):
+    	    attach_obj = self.pool.get('ir.attachment')
+            attachment_ids = attach_obj.search(cr, uid, [('res_model', '=', 'project.issue'), ('res_id', '=', issue_id)], context=context)
+
+	    return attach_obj.browse(cr, uid, attachment_ids, context=context)
+
+	def _save_attachments(datas_fname,datas):
+	    b64_fname = str(datas_fname)
+            fout = open(b64_fname, 'wb')
+            fout.write(datas.decode('base64'))
+            fout.close
+	    return True
+
+	def _add_attachment(attachment,isskey):
+	    att = self.pool.get('ir.attachment').browse(cr, SUPERUSER_ID, attachment.id, context=context)
+	    fi='/tmp/'+att.datas_fname
+	    sa = _save_attachments(fi, att.datas)
+
+	    if sa:
+		jira.add_attachment(isskey,fi,att.datas_fname)
+    		if os.path.exists(fi):
+            	    os.remove(fi)
+
+	    return True
+
+
+	def _handle_attachments(attachments, odoo_jira_attachments, isskey, issue_obj_update):
+	    attids=''
+            for attachment in attachments:
+		attids = str(attachment.id)+";"+attids
+
+            if attids:
+                attids=attids[:-1]
+
+            if odoo_jira_attachments:
+                logging.info('Attachment exists. Check and add only the new one!')
+
+                if odoo_jira_attachments != attids:
+                    logging.info('Attachments are NOT equals')
+                    attidtab=string.split(odoo_jira_attachments, ';')
+
+                    ###checking if attachment id is already saved in database
+                    for attachment in attachments:
+                        cexists=0
+                        for idtab in attidtab:
+                            if str(attachment.id)==str(idtab):
+                                cexists=1
+
+			if cexists==0:
+			    logging.info('Adding new attachment!')
+			    aa = _add_attachment(attachment,isskey)
+
+		    issue_obj_update.write(cr, uid, issueid.id, {'jira_attachments': attids}, context=context)
+	    else:
+		for attachment in attachments:
+		    aa = _add_attachment(attachment,isskey)
+
+		issue_obj_update.write(cr, uid, issueid.id, {'jira_attachments': attids}, context=context)
+
+	    return True
+
+
 	###get all issues from odoo - check if it was already transfered to jira.If no - transfer, if yes - check if there's anything to update.
 	for issueid in project_issue_ids:
 	    logging.info(issueid.id)
@@ -103,6 +217,11 @@ class project_jira(osv.osv):
 	    jira_id = odoo_issue.jira_id
 	    jira_key = odoo_issue.jira_key
 	    desc = odoo_issue.description
+	    priority = odoo_issue.priority
+	    odoo_jira_attachments = odoo_issue.jira_attachments
+
+	    if desc != "":
+		desc = name
 
 	    ##if jira_id and jira_key are empty it means that this is a new issue, which we need to transfer to jira
 	    if not jira_id and not jira_key:
@@ -112,6 +231,7 @@ class project_jira(osv.osv):
 	    	    'summary': str(name),
 	    	    'description': str(desc),
 	    	    'issuetype': {'name': 'Bug'},
+		    'priority': {'name': prioritymap[priority]},
 		}
 
 		new_issue = jira.create_issue(fields=issue_dict)
@@ -133,6 +253,20 @@ class project_jira(osv.osv):
 		issue_obj.write(cr, uid, issueid.id, {'jira_status': issstatus}, context=context)
 		issue_obj.write(cr, uid, issueid.id, {'jira_link': isslink}, context=context)
 
+
+                attachments = _get_attachments(issueid.id)
+		att = _handle_attachments(attachments,odoo_jira_attachments,isskey,issue_obj)
+
+##                for attachment in attachments:
+##		    att = self.pool.get('ir.attachment').browse(cr, SUPERUSER_ID, attachment.id, context=context)
+##		    fi='/tmp/filestore/'+att.datas_fname
+##		    sa = _save_attachments(fi, att.datas)
+
+##		    if sa:
+##			jira.add_attachment(isskey,fi,att.datas_fname)
+##			if os.path.exists(fi):
+##                    	    os.remove(fi)
+
 	    ##jira_id and jira_key are filled. Update issues...
 	    else:
 		logging.info('Issue exists in JIRA application. Doing update...')
@@ -140,32 +274,91 @@ class project_jira(osv.osv):
 		issue_to_update = jira.issue(jira_key)
 		jira_status = issue_to_update.fields.status.name
 		jira_description = issue_to_update.fields.description
+		jira_comments = [comment for comment in issue_to_update.fields.comment.comments]
 		odoo_jira_status = odoo_issue.jira_status
 		odoo_jira_description = odoo_issue.description
+		odoo_jira_comments = odoo_issue.jira_comments
+		odoo_jira_attachments = odoo_issue.jira_attachments
+		odoo_stage_id = odoo_issue.stage_id.id
 		
+		issue_obj_update = self.pool.get('project.issue')
+
+                attachments = _get_attachments(issueid.id)
+		att = _handle_attachments(attachments,odoo_jira_attachments,jira_key,issue_obj_update)
+
+##                for attachment in attachments:
+##		    att = self.pool.get('ir.attachment').browse(cr, SUPERUSER_ID, attachment.id, context=context)
+##		    fi='/tmp/filestore/'+att.datas_fname
+##		    sa = _save_attachments(fi, att.datas)
+
+##		    if sa:
+###			jira.add_attachment(jira_key,fi,att.datas_fname)
+##			if os.path.exists(fi):
+##                    	    os.remove(fi)
+
+		###Handling the comments
+		commids=''
+		for comment in jira_comments:
+		    commids = comment.id+";"+commids
+
+		if commids:
+		    commids=commids[:-1]
+
+		if odoo_jira_comments:
+		    logging.info('Comments exists. Check and add only the new one!')
+		    if odoo_jira_comments != commids:
+			logging.info('Comments are NOT equals')
+			commidtab=string.split(odoo_jira_comments, ';')
+
+			###checking if comment id is already saved in database
+	    		for comment in jira_comments:
+			    cexists=0
+			    for idtab in commidtab:
+				if comment.id==idtab:
+				    cexists=1
+			
+			    if cexists==0:
+				comment = str(comment.author)+":: "+str(comment.body)
+				issue_obj_update.message_post(cr, uid, issueid.id, body=_(comment), context=context)
+
+			issue_obj_update.write(cr, uid, issueid.id, {'jira_comments': commids}, context=context)
+		else:
+		    issue_obj_update.write(cr, uid, issueid.id, {'jira_comments': commids}, context=context)
+		    for comment in jira_comments:
+			comment = str(comment.author)+":: "+str(comment.body)
+			issue_obj_update.message_post(cr, uid, issueid.id, body=_(comment), context=context)
+
 		if jira_description != odoo_jira_description:
 		    ###if jira issue has no description it sends 'False' - need to eliminate this
 		    if jira_description != 'False':
 			###differences between descriptions. Doing update...(field+openchatter)
-			issue_obj_update = self.pool.get('project.issue')
 			issue_obj_update.write(cr, uid, issueid.id, {'description': jira_description}, context=context)
 			issue_obj_update.message_post(cr, uid, issueid.id, body=_("JIRA issue description has changed."), context=context)
 
+		task_type_name=self.get_task_type_name(cr, uid, odoo_stage_id, context=None)
+		current_odoo_stage = stagemap[task_type_name]
 
-		if str(jira_status) != str(odoo_jira_status):
+
+		if str(jira_status) != str(current_odoo_stage):
 		    ###differencess between statuses. Doing update...(field+openchatter)
-		    issue_obj_update = self.pool.get('project.issue')
+		    #issue_obj_update = self.pool.get('project.issue')
 		    issue_obj_update.write(cr, uid, issueid.id, {'jira_status': jira_status}, context=context)
 		    issue_obj_update.message_post(cr, uid, issueid.id, body=_("JIRA issue status has changed to "+str(jira_status)), context=context)
 
 		    task_type_id=self.get_task_id(cr, uid, jira_status, context=None)
-		    logging.info(task_type_id)
 		    if task_type_id:
 			###update issue status
 			issue_obj_update.write(cr, uid, issueid.id, {'stage_id': task_type_id}, context=context)
 
 		logging.info(odoo_issue.name)
 
+
+  def get_task_type_name(self, cr, uid, stage_id, context=None):
+    task_type = self.pool.get('project.task.type').search(cr, uid, [('id', '=', stage_id)])
+    if task_type:
+	task_type_name = self.pool.get('project.task.type').browse(cr, uid, task_type, context=context)
+
+    return task_type_name.name
 
 
   def get_task_id(self, cr, uid, jira_status, context=None):
